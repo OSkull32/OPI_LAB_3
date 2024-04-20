5868f9dae80454c6f2e311951d4a7cd52c1beea3
Владимир Данченко <368087@niuitmo.ru>
2024-04-20 11:13:58 +0300

    update test

--- src/test/java/ru/ifmo/web_lab_3/AreaResultTest.java
+++ src/test/java/ru/ifmo/web_lab_3/AreaResultTest.java
@@ -2,10 +2,100 @@ package ru.ifmo.web_lab_3;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class AreaResultTest {
+
+    @Test
+    public void topHitRight() {
+        double r = 5;
+        for (double x = 0; x <= 4; x += 0.1) {
+            for (double y = 0; y <= 2.5; y += 0.1) {
+                    boolean hit = AreaChecker.isHit(x, y, r);
+                    assertTrue(hit);
+            }
+        }
+    }
+
+    @Test
+    public void topMissRight() {
+        double r = 5;
+        for (double x = 0; x <= 4; x += 0.1) {
+            for (double y = 2.6; y <= 5; y += 0.1) {
+                boolean hit = AreaChecker.isHit(x, y, r);
+                assertFalse(hit);
+            }
+        }
+    }
+
+    @Test
+    public void downMissRight() {
+        double r = 5;
+        for (double x = 0.1; x <= 4; x += 0.1) {
+            for (double y = -5; y < 0; y += 0.1) {
+                boolean hit = AreaChecker.isHit(x, y, r);
+                assertFalse(hit);
+            }
+        }
+    }
+
+    @Test
+    public void downHitLeft() {
+        double r = 5;
+        for (double x = -4; x <= 0; x += 0.1) {
+            for (double y = -5; y <= 0; y += 0.1) {
+                if (- x - y <= r) {
+                    boolean hit = AreaChecker.isHit(x, y, r);
+                    assertTrue(hit);
+                }
+
+            }
+        }
+    }
+
+    @Test
+    public void downMissLeft() {
+        double r = 5;
+        for (double x = -4; x <= 0; x += 0.1) {
+            for (double y = -5; y <= 0; y += 0.1) {
+                if (- x - y > r) {
+                    boolean hit = AreaChecker.isHit(x, y, r);
+                    assertFalse(hit);
+                }
+
+            }
+        }
+    }
+
+    @Test
+    public void topHitLeft() {
+        double r = 5;
+        for (double x = -4; x <= 0; x += 0.1) {
+            for (double y = 0; y <= 5; y += 0.1) {
+                if (x * x + y * y <= r / 2 * r / 2) {
+                    boolean hit = AreaChecker.isHit(x, y, r);
+                    assertTrue(hit);
+                }
+
+            }
+        }
+    }
+
+    @Test
+    public void topMissLeft() {
+        double r = 5;
+        for (double x = -4; x <= 0; x += 0.1) {
+            for (double y = 0.1; y <= 5; y += 0.1) {
+                if (x * x + y * y > r / 2 * r / 2) {
+                    boolean hit = AreaChecker.isHit(x, y, r);
+                    assertFalse(hit);
+                }
+
+            }
+        }
+    }
+
 
     @Test
     void getResult() {
